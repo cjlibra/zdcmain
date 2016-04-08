b176e268c5ae7e63eb44207d7585856adbbb6162
cjlibra <cjlibra@qq.com>
2016-04-08 12:41:07 +0800

    添加webserver使得开发方便

---
+++ exe.bat
@@ -0,0 +1,6 @@
+
+cd %CD%
+set PATH=%PATH%;c:\go\bin;C:\Program Files\Git\bin
+set GOPATH=%CD%
+set GOARCH=386
+cmd
---
+++ webserver.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+
+	"fmt"
+	"net/http"
+
+	
+)
+
+
+
+
+func main() {
+	 
+
+	
+    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./"))))
+	fmt.Println("程序启动，开始监听80端口")
+
+
+	for {
+		err := http.ListenAndServe(":80", nil)
+		if err != nil {
+			//log.Fatal("ListenAndServer: ", err)
+			fmt.Println("ListenAndServer: ", err)
+
+		}
+	}
+
+}
+
