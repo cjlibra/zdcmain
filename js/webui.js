503852bd8043dc2d36f6e7a5fd5f42dcc352e91c
cjlibra <cjlibra@qq.com>
2016-05-04 11:07:22 +0800

    辉决定暂定一稿

--- js/webui.js
+++ js/webui.js
@@ -5,7 +5,28 @@
 			 $("#mylayer").css("height",viewheight+"px!important");
 			 alert(viewheight);
 			 */
-           
+			 
+			 // $("#tt").accordion('select',0);
+            // $("#sstab1").tabs("select", 1);
+			/* $('#stab1').tabs('update',{
+				tab:$('#stab1').tabs('getTab','DataGrid12'),
+				options:{
+				selected:true
+				}
+				});*/
+				
+				//$("#atab1").prop("data-options","selected:true");
+				//var tab = $('#tt').tabs('getSelected');
+				//$("#sstab1").tabs("getTab","DataGrid12").panel('refresh');
+				// $("#sstab1").tabs("select", 1);
+				
+				
+				$('#sstab1').tabs('update',{
+					tab:$('#sstab1').tabs('getTab','DataGrid12'),
+					options:{
+					selected:true
+					}
+					});
         });
 
        
